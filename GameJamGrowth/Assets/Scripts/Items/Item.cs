e9e992b65bd96e0ed6071958728f1365f7014216
Namu <49788407+namuuu@users.noreply.github.com>
2025-05-24 00:45:05 +0200

    feat: item sprite change mechanic

--- GameJamGrowth/Assets/Scripts/Items/Item.cs
+++ GameJamGrowth/Assets/Scripts/Items/Item.cs
@@ -1,15 +1,22 @@
 abstract class Item
 {
     public string Name { get; set; }
+    public string Id { get; set; }
     public string Description { get; set; }
-    public int Value { get; set; }
+    public ItemType ItemType { get; set; }
 
-    public Item(string name, string description, int value)
+    public Item(string name, string id, string description, ItemType itemType)
     {
         Name = name;
+        Id = id;
         Description = description;
-        Value = value;
+        ItemType = itemType;
     }
 
     public abstract void Use();
+}
+
+enum ItemType
+{
+    Seed,
 }
